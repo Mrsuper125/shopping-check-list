df3fc515e019d0a9da8e2cbc3d6738c73fb8c056
Mrsuper125 <Mrsuperr1221@gmail.com>
2023-01-14 12:12:13 +0300

    added recycler view

--- app/src/main/java/com/example/shoppingcheck_list/MainActivity.kt
+++ app/src/main/java/com/example/shoppingcheck_list/MainActivity.kt
@@ -2,10 +2,55 @@ package com.example.shoppingcheck_list
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.appcompat.view.menu.MenuView.ItemView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+public data class Item(val constructorName : String){
+    val name = constructorName
+}
+
+class RecyclerAdapter(private val items: List<Item>) :
+    RecyclerView.Adapter<RecyclerAdapter.RecyclerViewHolder>() {
+
+    class RecyclerViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        val item:LinearLayout = itemView.findViewById(R.id.productLayout)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.product, parent, false)
+        return RecyclerViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: RecyclerViewHolder, position: Int) {
+        val item = holder.item
+        item.findViewById<TextView>(R.id.productName).text = items[position].name
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+}
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var dicks = mutableListOf<Item>()
+
+        for (i in 10..30){
+            dicks.add(Item("Дилдо длиной "+i+" см"))
+        }
+
+        val recyclerView: RecyclerView = findViewById(R.id.productList)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = RecyclerAdapter(dicks)
+
     }
 }
